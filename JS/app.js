0475133fd74ac1e07f032deb94168e076b5304d7
Ralph Warnes <ralphwarnesph60@gmail.com>
2022-11-08 22:17:34 -0300

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,5 @@
+let saldo = 100000.0;
+
 class Product {
   constructor(id, nombre, precio, stock, categoria) {
     this.id = parseInt(id);
@@ -7,144 +9,122 @@ class Product {
     this.category = categoria;
   }
 
-  comprarProducto() {
-    console.log(`${this.name} comprado - ${this.price}`);
+  comprar() {
+    if (chekearSaldo(this.price)) {
+      saldo = saldo - this.price;
+      this.stock = this.stock - 1;
+      alert(`Compra Exitosa!
+          x1 ${this.name}, $${this.price}
+          Saldo restante $${saldo}`);
+    } else {
+      alert("saldo insuficiente.");
+    }
   }
 }
 
-let productos = [
-  {
-    id: 1,
-    name: "Audient Sono",
-    price: 89129.0,
-    stock: 3,
-    category: "Interfaces de Audio",
-  },
-  {
-    id: 2,
-    name: "Audient ID4 mk2",
-    price: 80300.0,
-    stock: 2,
-    category: "Interfaces de Audio",
-  },
-  {
-    id: 3,
-    name: "Audient ID14 mk2",
-    price: 115500.0,
-    stock: 3,
-    category: "Interfaces de Audio",
-  },
-  {
-    id: 4,
-    name: "Black Lion Revolution 2x2",
-    price: 89400.0,
-    stock: 3,
-    category: "Interfaces de Audio",
-  },
-  {
-    id: 5,
-    name: "EVO By Audient EVO 8",
-    price: 79200.0,
-    stock: 3,
-    category: "Interfaces de Audio",
-  },
-  {
-    id: 6,
-    name: "Lynx Studio Technology Hilo USB",
-    price: 1157200.0,
-    stock: 3,
-    category: "Interfaces de Audio",
-  },
-  {
-    id: 7,
-    name: "Aston Spirit",
-    price: 111701.0,
-    stock: 3,
-    category: "microfonos",
-  },
-  {
-    id: 8,
-    name: "Warm Audio WA251",
-    price: 325600.0,
-    stock: 3,
-    category: "microfonos",
-  },
-  {
-    id: 9,
-    name: "Soyuz 023 Bomblet Deluxe",
-    price: 646800.0,
-    stock: 3,
-    category: "microfonos",
-  },
-  {
-    id: 10,
-    name: "Avantone Audio CK-1",
-    price: 63800.0,
-    stock: 3,
-    category: "microfonos",
-  },
-  {
-    id: 11,
-    name: "Soyuz 013 FET",
-    price: 277200.0,
-    stock: 3,
-    category: "microfonos",
-  },
-  {
-    id: 12,
-    name: "Telefunken M60",
-    price: 297000.0,
-    stock: 3,
-    category: "microfonos",
-  },
-  {
-    id: 13,
-    name: "Sony MDR7506",
-    price: 60500.0,
-    stock: 3,
-    category: "auriculares",
-  },
-  {
-    id: 14,
-    name: "Avantone Audio Mixphones MP1",
-    price: 88000.0,
-    stock: 3,
-    category: "auriculares",
-  },
-  {
-    id: 15,
-    name: "512 AUDIO Academy",
-    price: 45100.0,
-    stock: 3,
-    category: "auriculares",
-  },
-  {
-    id: 16,
-    name: "Ultrasone Signature Studio",
-    price: 156200.0,
-    stock: 3,
-    category: "auriculares",
-  },
-];
+let producto1 = new Product(
+  1,
+  "Audient Sono",
+  89129.0,
+  3,
+  "Interfaces de Audio"
+);
+let producto2 = new Product(
+  2,
+  "Audient ID4 mk2",
+  80300.0,
+  3,
+  "Interfaces de Audio"
+);
+let producto3 = new Product(
+  3,
+  "Audient ID14 mk2",
+  115500.0,
+  3,
+  "Interfaces de Audio"
+);
+let producto4 = new Product(
+  4,
+  "Black Lion Revolution 2x2",
+  89400.0,
+  3,
+  "Interfaces de Audio"
+);
+let producto5 = new Product(
+  5,
+  "EVO By Audient EVO 8",
+  79200.0,
+  3,
+  "Interfaces de Audio"
+);
+let producto6 = new Product(
+  6,
+  "Lynx Studio Technology Hilo USB",
+  1157200.0,
+  3,
+  "Interfaces de Audio"
+);
+let producto7 = new Product(7, "Aston Spirit", 1157200.0, 3, "microfonos");
+let producto8 = new Product(8, "Warm Audio WA251", 1157200.0, 3, "microfonos");
+let producto9 = new Product(
+  9,
+  "Soyuz 023 Bomblet Deluxe",
+  1157200.0,
+  3,
+  "microfonos"
+);
+let producto10 = new Product(
+  10,
+  "Avantone Audio CK-1",
+  1157200.0,
+  3,
+  "microfonos"
+);
+let producto11 = new Product(11, "Soyuz 013 FET", 1157200.0, 3, "microfonos");
+let producto12 = new Product(12, "Sony MDR7506", 60500.0, 3, "auriculares");
+let producto13 = new Product(
+  12,
+  "Avantone Audio Mixphones MP1",
+  88000.0,
+  3,
+  "auriculares"
+);
+let producto14 = new Product(
+  12,
+  "512 AUDIO Academy",
+  45100.0,
+  3,
+  "auriculares"
+);
+let producto15 = new Product(
+  12,
+  "Ultrasone Signature Studio",
+  156200.0,
+  3,
+  "auriculares"
+);
+
+let productos = [];
+productos.push(producto1);
+productos.push(producto2);
+productos.push(producto3);
+productos.push(producto4);
+productos.push(producto5);
+productos.push(producto6);
+productos.push(producto7);
+productos.push(producto8);
+productos.push(producto9);
+productos.push(producto10);
+productos.push(producto11);
+productos.push(producto12);
+productos.push(producto13);
+productos.push(producto14);
+productos.push(producto15);
 
 let producto = new Product("Auricular", 10, 3);
 
-console.log(producto.price);
-
-//const producto1 = "Auricular";
-//const producto2 = "tecaldo midi";
-//const producto3 = "Interfaz";
-//const producto4 = "Microfono";
-
-//let producto1Precio = 10;
-//let producto2Precio = 15;
-//let producto3Precio = 20;
-//let producto4Precio = 25;
-
-let saldo = 2000000;
 let opcionElegida;
 let opcion2;
-let opcion3;
 
 // CICLO PRINCIPAL PARA NAVEGAR ENTRE LAS DISTINTAS FUNCIONES
 do {
@@ -155,7 +135,7 @@ do {
   4. Salir.
   `);
 
-  if (opcionElegida == "1") {
+  if (opcionElegida === "1") {
     menuCompra();
   }
   if (opcionElegida == "2") {
@@ -175,59 +155,39 @@ do {
 function menuCompra() {
   do {
     let textoFinal = "";
-    let precioProductos = productos.forEach((product) => {
-      objProduct = new Product(
-        product.id,
-        product.name,
-        product.price,
-        product.stock,
-        product.category
-      );
-      let proudtoTexto = `
-      ${objProduct.id}. ${objProduct.name} - $${objProduct.price}`;
-
-      textoFinal = textoFinal + proudtoTexto;
+    productos.forEach((product) => {
+      if (product.stock > 0) {
+        let proudtoTexto = `
+            ${product.id}. ${product.name} Stock: ${product.stock} - $${product.price} `;
+        textoFinal = textoFinal + proudtoTexto;
+      }
     });
 
     let opcion2 = prompt(` 
-    |EMPIRE SOUNDS|
-   Tu saldo es de $${saldo}
-   Seleciona tu producto.
-   
-   ${textoFinal}
-
-   E. Volver al MENU PRINCIPAL.
+            |EMPIRE SOUNDS|
+           Tu saldo es de $${saldo}
+           Seleciona tu producto.
+           
+           ${textoFinal}
+        
+           E. Volver al MENU PRINCIPAL.
    `);
 
     if (opcion2 != "E") {
-      productElegido = productos.find((prduct) => prduct.id == opcion2);
-      objProduct = new Product(
-        productElegido.id,
-        productElegido.name,
-        productElegido.price,
-        productElegido.stock,
-        productElegido.category
-      );
+      let productElegido = productos.find((product) => product.id == opcion2);
+      productElegido.comprar();
+    }
 
-      objProduct.comprarProducto();
+    if (opcion2 == "E") {
+      break;
     }
   } while (opcion2 != "E");
 }
 
-function comprarProducto(nombre, precio) {
-  //calculo para actualizar el saldo luego de una compra x
-  if (chekearSaldo(saldo, precio)) {
-    saldo = saldo - precio;
-    alert(`Compra Exitosa!
-          x1 ${nombre}, $${precio}
-          Saldo restante $${saldo}`);
-  } else {
-    alert("saldo insuficiente.");
-  }
-}
-
 //CORROBORA QUE EL VALOR INGRESADO SEA VALIDO
-function chekearSaldo(saldo, precio) {
+function chekearSaldo(precio) {
+  1;
+
   if (saldo < precio) {
     return false;
   }
